aac9eb1d35a53b9286f3a33c690f65fc12181413
armfazh <armfazh@cloudflare.com>
2024-04-12 17:54:08 -0700

    Removing buffer.

--- test/conv.ts
+++ test/conv.ts
@@ -1,19 +1,22 @@
-import { Buffer } from 'node:buffer';
-
-export function hexNumToB64URL(x: string): string {
-    if (x.startsWith('0x')) {
-        x = x.slice(2);
-    }
-    return Buffer.from(x, 'hex').toString('base64url');
-}
-
 export function hexToUint8(x: string): Uint8Array {
     if (x.startsWith('0x')) {
-        x = x.slice(2);
+        x = x.substring(2);
     }
-    return new Uint8Array(Buffer.from(x, 'hex'));
+    return Uint8Array.from({ length: Math.floor(x.length / 2) }, (_, i) =>
+        parseInt(x.substring(2 * i, 2 * i + 2), 16),
+    );
 }
 
 export function uint8ToHex(x: Uint8Array): string {
-    return Buffer.from(x).toString('hex');
+    return Array.from(x)
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('');
+}
+
+function b64ToB64URL(s: string) {
+    return s.replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+export function hexNumToB64URL(x: string): string {
+    return b64ToB64URL(btoa(String.fromCharCode(...hexToUint8(x))));
 }
